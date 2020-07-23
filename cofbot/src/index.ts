f8f6acd496f4a7af005d264d1c6abe967edcd65c
dcsan <dc@rikai.co>
2020-07-23 16:16:21 -0700

    working version with botkit but tsnode config doesnt work :(

--- cofbot/src/index.ts
+++ cofbot/src/index.ts
@@ -1,10 +1,11 @@
-// This is the main file for the cbgbot bot.
+// This is the main file for the cbgbot bot
+import morgan from 'morgan'
 
 // Import Botkit's core features
 import { Botkit } from 'botkit'
-import { BotkitCMSHelper } from 'botkit-plugin-cms'
+// import { BotkitCMSHelper } from 'botkit-plugin-cms'
 
-// Import a platform-specific adapter for slack.
+// Import a platform-specific adapter for slack
 import { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } from 'botbuilder-adapter-slack'
 import { MongoDbStorage } from 'botbuilder-storage-mongodb'
 
@@ -12,8 +13,10 @@ import { MongoDbStorage } from 'botbuilder-storage-mongodb'
 import { config } from 'dotenv'
 config()
 
-console.log('using MONGO_URI', process.env.MONGO_URI)
-console.log('using NODE_ENV', process.env.NODE_ENV)
+const Logger = console
+
+Logger.log('using MONGO_URI', process.env.MONGO_URI)
+Logger.log('using NODE_ENV', process.env.NODE_ENV)
 
 let storage = undefined
 if (process.env.MONGO_URI) {
@@ -25,9 +28,23 @@ if (process.env.MONGO_URI) {
   // mongoStorage = storage
 }
 
+const AppConfig = {
+  verificationToken: process.env.VERIFICATION_TOKEN,
+  clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+
+  // auth token for a single-team app
+  botToken: process.env.BOT_TOKEN,
+
+  // credentials used to set up oauth for multi-team apps
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
+}
+
 const adapter = new SlackAdapter({
   // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
   enable_incomplete: true,
+  ...AppConfig,
 
   // parameters used to secure webhook endpoint
   verificationToken: process.env.VERIFICATION_TOKEN,
@@ -39,9 +56,9 @@ const adapter = new SlackAdapter({
   // credentials used to set up oauth for multi-team apps
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  scopes: ['bot'],
   redirectUri: process.env.REDIRECT_URI,
 
+  scopes: ['bot'],
   // functions required for retrieving team-specific info
   // for use in multi-team apps
 
@@ -50,6 +67,8 @@ const adapter = new SlackAdapter({
   // @ts-ignore
   getBotUserByTeam: getBotUserByTeam,
 })
+
+Logger.log('AppConfig', JSON.stringify(AppConfig, null, 2))
 
 // Use SlackEventMiddleware to emit events that match their original Slack event types.
 adapter.use(new SlackEventMiddleware())
@@ -63,15 +82,19 @@ const controller = new Botkit({
   storage,
 })
 
-if (process.env.CMS_URI) {
-  controller.usePlugin(
-    new BotkitCMSHelper({
-      uri: process.env.CMS_URI,
-      // @ts-ignore
-      token: process.env.CMS_TOKEN,
-    })
-  )
-}
+// enable logging
+controller.webserver.use(morgan('short'))
+controller.middleware.receive.use(morgan('short'));
+
+// if (process.env.CMS_URI) {
+//   controller.usePlugin(
+//     new BotkitCMSHelper({
+//       uri: process.env.CMS_URI,
+//       // @ts-ignore
+//       token: process.env.CMS_TOKEN,
+//     })
+//   )
+// }
 
 // Once the bot has booted up its internal services
 controller.ready(() => {
@@ -94,6 +117,7 @@ controller.ready(() => {
 })
 
 controller.webserver.get('/', (_req, res) => {
+  Logger.log('get /')
   const output = {
     msg: `This app is running Botkit ${ controller.version }.`,
     ver: '0.0.2x',
@@ -111,7 +135,7 @@ controller.webserver.get('/install/auth', async (req, res) => {
   try {
     const results = await controller.adapter.validateOauthCode(req.query.code)
 
-    console.log('FULL OAUTH DETAILS', results)
+    Logger.log('FULL OAUTH DETAILS', results)
 
     // Store token by team in bot state.
     tokenCache[results.team_id] = results.bot.bot_access_token
@@ -121,7 +145,7 @@ controller.webserver.get('/install/auth', async (req, res) => {
 
     res.json('Success! Bot installed.')
   } catch (err) {
-    console.error('OAUTH ERROR:', err)
+    Logger.error('OAUTH ERROR:', err)
     res.status(401)
     res.send(err.message)
   }
@@ -132,10 +156,12 @@ let userCache = {}
 
 if (process.env.TOKENS) {
   tokenCache = JSON.parse(process.env.TOKENS)
+  Logger.log('found env.tokens', process.env.TOKENS)
 }
 
 if (process.env.USERS) {
   userCache = JSON.parse(process.env.USERS)
+  Logger.log('found env.USERS', process.env.USERS)
 }
 
 async function getTokenForTeam(teamId) {
@@ -146,7 +172,7 @@ async function getTokenForTeam(teamId) {
       }, 150)
     })
   } else {
-    console.error('Team not found in tokenCache: ', teamId)
+    Logger.error('Team not found in tokenCache: ', teamId)
   }
 }
 
@@ -158,7 +184,7 @@ async function getBotUserByTeam(teamId) {
       }, 150)
     })
   } else {
-    console.error('Team not found in userCache: ', teamId)
+    Logger.error('Team not found in userCache: ', teamId)
   }
 }
 
