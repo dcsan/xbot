daf2e9306b058844d3aa7cc918787c0150355ae4
dcsan <dc@rikai.co>
2020-09-23 05:17:22 -0700

    tests

--- server/src/mup/nlp/DfWrapper.test.ts
+++ server/src/mup/nlp/DfWrapper.test.ts
@@ -20,7 +20,7 @@ test('simple query', async () => {
 
 test('more complex', async () => {
   const dfResult: INlpResult = await dfQuery('look inside the desk')
-  logger.logObj('dfresult', dfResult, { force: true })
+  // logger.logObj('dfresult', dfResult, { force: true })
   expect(dfResult.intentName).toBe('openThing')
   expect(dfResult.entities[0].name).toBe('roomthing')
   expect(dfResult.entities[0].value).toBe('desk')
@@ -28,11 +28,9 @@ test('more complex', async () => {
   expect(dfResult.textResponse).toBe('open desk')
 })
 
-
-
 test('more complex 2', async () => {
   const dfResult: INlpResult = await dfQuery('take the poster off the wall')
-  logger.logObj('dfresult', dfResult, { force: true })
+  // logger.logObj('dfresult', dfResult, { force: true })
   expect(dfResult.intentName).toBe('getThing')
   expect(dfResult.entities[0].name).toBe('roomthing')
   expect(dfResult.entities[0].value).toBe('poster')
